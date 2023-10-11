59958c97998c5b075324b40b22e0d9fa1b50696a
Hamzah <hamzahchhapra2004@gmail.com>
2023-10-11 21:22:01 +0530

    react-lecture-1 complete

---
+++ first-react-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+function Hamzah() {
+  return (
+    <a 
+    href='https://hamzahxx.github.io' 
+    className='App-link'
+    target='_blank'
+    >
+      hamzah.devs
+    </a>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          first react app
+        </h2>
+        <Hamzah />
+      </header>
+    </div>
+  );
+}
+
+export default App;
