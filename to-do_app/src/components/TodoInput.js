7e56c2cb3069d46096c2cf43bd5846e2d14f16a4
Hamzah <hamzahchhapra2004@gmail.com>
2023-10-18 01:33:39 +0530

    done for the day
    watched states video till 6:39 but start again tomorrow

---
+++ to-do_app/src/components/TodoInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TodoInput() {
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Work to be done"
+        />
+        <button className="btn custom-nav-button">
+          Pass it
+        </button>
+      </div>
+    </div>
+  );
+}
