1690f9e50971d99a8fcf2c148b922f84bf15ff65
Hamzah <hamzahchhapra2004@gmail.com>
2023-10-20 21:36:29 +0530

    added a new div for todo-item-list

--- to-do_app/src/components/TodoInput.js
+++ to-do_app/src/components/TodoInput.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoInput() {
   return (
+    <>
       <div className="input-group mb-3">
         <input
+          id="InPut"
           type="text"
           className="form-control"
           placeholder="Work to be done"
+          required
         />
-        <button className="btn custom-nav-button">
+        <button /* onClick={} */ className="btn custom-nav-button">
           Pass it
         </button>
       </div>
+      <div>
+        <ul id="todo-list"></ul>
+      </div>
+    </>
   );
 }
+
+// let liElements = document.querySelectorAll("li.todo-task")
+// let num = 1
+// function AddTodo() {
+//   const input = document.getElementById("InPut").value;
+//   const listNode = document.createElement("li");
+//   listNode.innerHTML = input;
+//   listNode.setAttribute("id", `todo-task-${num}`);
+//   listNode.setAttribute("class", "todo-task")
+//   document.getElementById("todo-list").appendChild(listNode);
+//   num++;
+// }
